465407357479c198c07c7a71c3947b4f72320a3d
angelawu236 <anw041@ucsd.edu>
2025-09-02 00:15:22 -0700

    added more stuff to contact, not dynamic

--- lib/sections/landing.dart
+++ lib/sections/landing.dart
@@ -7,67 +7,140 @@ class Landing extends StatelessWidget {
   const Landing({super.key});
 
   @override
-
   Widget build(BuildContext context) {
-    TextSpan span(String text, [Color? color]) =>
-        TextSpan(text: text, style: color != null ? TextStyle(color: color) : null);
-
+    TextSpan span(String text, [Color? color]) => TextSpan(
+        text: text, style: color != null ? TextStyle(color: color) : null);
 
     return AppCard(
-        child: Column(
-      children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.blueAccent.withOpacity(0.5), // change border color here
-                  width: 10,            // change thickness here
-                ),
-                borderRadius: BorderRadius.circular(6), // optional rounded corners
-              ),
-              child: ClipRRect(
-                child: Image.asset(
-                  "assets/profile2.jpg",
-                  fit: BoxFit.cover,
-                  width: 300,
-                  height: 300,
-                ),
-              ),
-            ),
-            SizedBox(width: 20),
-            Expanded(
-              child: Container(
-                child: RichText(
-                  text: TextSpan(
-                    style: TextStyle(fontSize: 17, color: Colors.white, fontFamily: GoogleFonts.firaCode().fontFamily,),
-                    children: [
-                      span("Angela Wu is a "),
-                      span("Mathematics & Computer Science", Colors.cyanAccent),
-                      span(" Major at"),
-                      span(" UC San Diego", Colors.greenAccent),
-                      span(" (2027), minoring in"),
-                      span(" Data Science.", Colors.cyanAccent),
-                      span(" She is"),
-                      span(" passionate", Colors.purpleAccent),
-                      span(" about", Colors.lightBlueAccent),
-                      span(" using", Colors.greenAccent),
-                      span(" technology", Colors.yellowAccent),
-                      span(" creat", Colors.orangeAccent),
-                      span("ively", Colors.redAccent),
-                      span(" and has dabbled in full-stack web and mobile applications, embededded software systems, and more. "),
-                      span("Click & scroll around to learn more!")
-                    ],
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(minHeight: 300, maxHeight: 300),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blueAccent
+                        .withOpacity(0.5), // change border color here
+                    width: 10, // change thickness here
                   ),
-                )
+                  borderRadius:
+                      BorderRadius.circular(6), // optional rounded corners
+                ),
+                child: ClipRRect(
+                  child: Image.asset(
+                    "assets/profile2.jpg",
+                    fit: BoxFit.cover,
+                    width: 300,
+                    height: 300,
+                  ),
+                ),
+              ),
+              SizedBox(width: 20),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                        child: RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.white,
+                          fontFamily: GoogleFonts.firaCode().fontFamily,
+                        ),
+                        children: [
+                          span("Angela Wu is a "),
+                          span("Mathematics & Computer Science", Colors.cyanAccent),
+                          span(" Major at"),
+                          span(" University of California, San Diego",
+                              Colors.greenAccent),
+                          span(" (2027), minoring in"),
+                          span(" Data Science.", Colors.cyanAccent),
+                          span(" She is"),
+                          span(" passionate", Colors.purpleAccent),
+                          span(" about", Colors.lightBlueAccent),
+                          span(" using", Colors.greenAccent),
+                          span(" technology", Colors.yellowAccent),
+                          span(" creat", Colors.orangeAccent),
+                          span("ively", Colors.redAccent),
+                          span(
+                              " and has dabbled in full-stack web and mobile applications, embedded software systems, and more. "),
+                          span("Click & scroll around to learn more!")
+                        ],
+                      ),
+                    )),
+                    Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.lightBlueAccent,
+                            borderRadius:
+                            BorderRadius.circular(6), // optional rounded corners
+                          ),
+                          height: 50,
+                          width: 200,
+                          child: Center(
+                            child: Text(
+                                "- LinkedIn -",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
 
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.lightBlueAccent.withOpacity(0.8),
+                            borderRadius:
+                            BorderRadius.circular(6), // optional rounded corners
+                          ),
+                          height: 50,
+                          width: 200,
+                          child: Center(
+                            child: Text(
+                              "- Github -",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.lightBlueAccent.withOpacity(0.5),
+                            borderRadius:
+                            BorderRadius.circular(6), // optional rounded corners
+                          ),
+                          height: 50,
+                          width: 200,
+                          child: Center(
+                            child: Text(
+                              "- Contact Me -",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 30),
+                  ],
+                ),
 
               ),
-            ),
-          ],
-        ),
-      ],
-    ));
+
+
+            ],
+          ),
+        ));
   }
 }
